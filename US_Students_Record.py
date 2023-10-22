b5bebd1ed887be0f035ffa864c19b2f06e9a16b2
Abhi14kr <abhinav14kr@zohomail.com>
2023-10-22 14:46:19 -0700

    International Student Population in the United States

--- US_Students_Record.py
+++ US_Students_Record.py
@@ -8,10 +8,10 @@ import pandas as pd
 
 file_path = 'C:/Users/Abhinav/Desktop/Data Analytics/US_Students_Records.xlsx'
 
-# Load the XLSX file
+# Loading the XLSX file
 record = pd.read_excel(file_path)
 
-# Display the first few rows to verify the data was loaded
+# Displaying the first few rows to verify the data was loaded
 df.head()
 
 
@@ -22,14 +22,14 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Create a line plot to show the trend over the years.
+# Creating a line plot to show the trend over the years.
 plt.figure(figsize=(12, 6))
 ax = sns.lineplot(data=record, x='Year', y='Total', marker='o', markersize=5, color='b', label='Total Students')
 plt.title('Trend of Total International Students from All Continents Going to the USA')
 plt.xlabel('Year')
 plt.ylabel('Total Number of Students')
 
-# Set x-axis ticks at 5-year intervals (adjust based on your dataset)
+# Setting x-axis ticks at 5-year intervals
 xticks = range(0, len(record['Year']), 5)
 xlabels = record['Year'].iloc[xticks].apply(lambda x: x.replace('/', '-'))
 ax.set_xticks(xticks)
@@ -47,17 +47,15 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Assuming your dataset is already loaded and named as 'record'.
-
-# Create a line plot for the selected continents.
+# Creating a line plot for the selected continents.
 selected_continents = [
     'Asia', 'East_Asia', 'South_and_Central_Asia', 'Southeast_Asia'
 ]
 
-# Define the years for custom x-axis labels.
+# Defining the years for custom x-axis labels.
 custom_years = ['1949-50', '1974-75', '1999-00', '2004-05', '2009-10', '2014-15', '2019-20']
 
-# Create a mapping from custom years to the closest available data points.
+# Creating a mapping from custom years to the closest available data points.
 custom_years_to_data_index = {
     '1949-50': 0,
     '1974-75': 5,
@@ -78,10 +76,10 @@ plt.ylabel('Number of Students')
 plt.legend(loc='upper left')
 plt.grid(True)
 
-# Expand the vertical axis.
+# Expanding the vertical axis.
 plt.ylim(bottom=0)
 
-# Set custom x-axis ticks and add labels at the end of each line.
+# Setting custom x-axis ticks and add labels at the end of each line.
 plt.xticks([custom_years_to_data_index[year] for year in custom_years], custom_years, rotation=45)
 
 for column in selected_continents:
@@ -99,23 +97,21 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Assuming your dataset is already loaded and named as 'record'.
-
-# Set a Seaborn style with a dark grey background
+# Setting a Seaborn style with a dark grey background
 sns.set(style="darkgrid")
 
-# Create a color palette
+# Creating a color palette
 colors = sns.color_palette("husl")
 
-# Create a line plot for the selected continents.
+# Creating a line plot for the selected continents.
 selected_continents = [
     'Asia', 'East_Asia', 'South_and_Central_Asia', 'Southeast_Asia'
 ]
 
-# Define the years for custom x-axis labels.
+# Defining the years for custom x-axis labels.
 custom_years = ['1949-50', '1974-75', '1999-00', '2004-05', '2009-10', '2014-15', '2019-20']
 
-# Create a mapping from custom years to the closest available data points.
+# Creating a mapping from custom years to the closest available data points.
 custom_years_to_data_index = {
     '1949-50': 0,
     '1974-75': 5,
@@ -126,7 +122,7 @@ custom_years_to_data_index = {
     '2019-20': 30
 }
 
-# Create a dark grey background
+# Creating a dark grey background
 plt.figure(figsize=(12, 6))
 ax = plt.gca()
 ax.set_facecolor('#333333')  # Dark grey background
@@ -140,13 +136,13 @@ plt.ylabel('Number of Students')
 plt.legend(loc='upper left')
 plt.grid(True, color='white', linestyle='--')
 
-# Expand the vertical axis.
+# Expanding the vertical axis.
 plt.ylim(bottom=0)
 
-# Set custom x-axis ticks and add labels at the end of each line.
+# Setting custom x-axis ticks and add labels at the end of each line.
 plt.xticks([custom_years_to_data_index[year] for year in custom_years], custom_years, rotation=45)
 
-# Add values to the x-axis labels
+# Adding values to the x-axis labels
 for year in custom_years:
     x_pos = custom_years_to_data_index[year]
     for column in selected_continents:
